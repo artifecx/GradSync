7f1d97526038cfd611dc36a0406e29adbc40ded1
CharlotteTaboada <charlotte.taboada@cit.edu>
2024-08-05 05:49:11 +0800

    Cards and Job Application UI Things

--- frontend/src/components/AppliedJobCard.jsx
+++ frontend/src/components/AppliedJobCard.jsx
@@ -1,67 +1,57 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import { Link } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux'
 
 
 export const AppliedJobCard = ({id, job, time }) => {
-
+    
     const dispatch = useDispatch()
 
-    const convertDateFormat = (inputDate) => {
-        const parts = inputDate.split('-');
-        if (parts.length !== 3) {
-            return "Invalid date format";
-        }
-
-        const day = parts[2];
-        const month = parts[1];
-        const year = parts[0];
-
-        return `${day}-${month}-${year}`;
+  const convertDateFormat = (inputDate) => {
+    const parts = inputDate.split('-');
+    if (parts.length !== 3) {
+      return "Invalid date format";
     }
 
-   
+    const day = parts[2];
+    const month = parts[1];
+    const year = parts[0];
 
-    
+    const monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+    return `${monthNames[parseInt(month) - 1]} ${day}, ${year}`;
+  };
 
+  const truncateDescription = (description, maxLength) => {
+    if (description.length > maxLength) {
+      return `${description.substring(0, maxLength)}...`;
+    }
+    return description;
+  };
 
-    return (
-        <div className='text-[#743030] flex flex-col gap-2 rounded-[10px] md:px-6 px- w-full py-6'>
+  const capitalizeWords = (str) => {
+    return str.replace(/\b\w/g, char => char.toUpperCase());
+  };
 
-            <div className='flex gap-5 relative'>
-                <div className='flex justify-center items-center '>
-                    <img src={job.companyLogo.url} className='md:w-[10em] md:h-[10em]' alt="" />
-                </div>
-                <div className='flex flex-col '>
-
-                    <div>
-                        <p className='md:text-4xl font-bold'>{job.title}</p>
-                    </div>
-                    <div className='flex justify-between gap-2 '>
-                        <div className='flex flex-col gap-1'>
-                            <p className='text-sm font-medium'>{job.companyName}</p>
-                            <p className='text-sm'>{job.exp}</p>
-                            <p className='text-sm md:flex hidden'>{job.description.slice(0, 90)}...</p>
-                            <p className='text-sm flex md:hidden'>{job.description.slice(0, 25)}...</p>
-                        </div>
-                        <div className='absolute md:right-3 right-0 md:pt-0 md:top-3 top-18  flex flex-col gap-3' >
-                            <Link to={`/Application/Details/${id}`} className='"btn bg-[#7A1515] hover:[#A53D3D] text-white font-semibold py-2 px-4 rounded inline-block'>View Application</Link>
-                            
-                        </div>
-
-
-                    </div>
-
-                </div>
-
-
+  return (
+    <div className='text-black shadow-sm bg-[#f3f3f3] rounded-lg px-4 py-2 flex flex-col gap-2 w-full'>
+      <div className='flex justify-between items-center'>
+        <div className='flex-1 ml-4'>
+          <div>
+            <h3 className="text-xl font-bold uppercase">{job.title}</h3>
+          </div>
+          <div className='flex justify-between gap-2'>
+            <div className='flex flex-col gap-1'>
+              <p className='text-sm font-semibold'>{capitalizeWords(job.employmentType)} | {job.companyName} - {job.location}</p>
+              <p className='text-sm'>{job.exp}</p>
+              <p className="text-sm mt-2" style={{ whiteSpace: 'pre-wrap' }}>{truncateDescription(job.description, 200)}</p>
             </div>
-
-            <div className='flex md:gap-8 gap-3 md:text-sm text-xs'>
-                <span>Applied on {convertDateFormat(time.substr(0, 10))}</span>
-               
-            </div>
-
+          </div>
         </div>
-    )
-}
+        <div className="text-xs text-gray-600 self-start">Applied On: {convertDateFormat(time.substr(0, 10))}</div>
+      </div>
+      <div className="flex items-center mb-4 mt-2 pl-4">
+        <Link to={`/Application/Details/${id}`} className="text-xs font-semibold mr-2 bg-[#7A1515] hover:bg-yellow-500 text-white py-2 px-4 rounded-sm w-max">View Application</Link>
+      </div>
+    </div>
+  );
+};
--- frontend/src/pages/ApplicationDetails.jsx
+++ frontend/src/pages/ApplicationDetails.jsx
@@ -3,20 +3,16 @@ import { useParams } from 'react-router'
 import { useDispatch, useSelector } from 'react-redux'
 import { Loader } from '../components/Loader'
 import { MetaData } from '../components/MetaData'
-import { getSingleApplication } from '../actions/ApplicationActions'
+import { getSingleApplication, deleteApplication } from '../actions/ApplicationActions'
 import { Link } from 'react-router-dom'
 import { TbLoader2 } from 'react-icons/tb'
-import {deleteApplication} from '../actions/ApplicationActions'
 import { useNavigate } from 'react-router'
 
 export const ApplicationDetails = () => {
-
     const { applicationDetails, loading } = useSelector(state => state.application)
     const dispatch = useDispatch()
     const navigate = useNavigate()
-
     const { id } = useParams()
-
 
     const deleteApplicationHandler = () => {
         dispatch(deleteApplication(id))
@@ -25,136 +21,92 @@ export const ApplicationDetails = () => {
 
     useEffect(() => {
         dispatch(getSingleApplication(id))
-        
-    }, [])
-
+    }, [dispatch, id])
 
     const toUpperFirst = (str = "") => {
         return str.substring(0, 1).toUpperCase() + str.substring(1)
     }
 
-
     const convertDateFormat = (inputDate) => {
-        const parts = inputDate.split('-');
+        const parts = inputDate.split('-')
         if (parts.length !== 3) {
-            return "Invalid date format";
+            return "Invalid date format"
         }
-
-        const day = parts[2];
-        const month = parts[1];
-        const year = parts[0];
-
-        return `${day}-${month}-${year}`;
+        const day = parts[2]
+        const month = parts[1]
+        const year = parts[0]
+        return `${day}-${month}-${year}`
     }
 
-    function extractTime(inputString) {
-        // Parse the input string as a Date object
-        const dateTimeObj = new Date(inputString);
-
-        // Extract the hours, minutes, and seconds
-        const hours = dateTimeObj.getHours();
-        const minutes = dateTimeObj.getMinutes();
-        const seconds = dateTimeObj.getSeconds();
-
-        // Convert to 12-hour format
-        const period = hours >= 12 ? 'PM' : 'AM';
-        const hours12 = hours % 12 || 12;
-
-        // Format the time
-        const time12hr = `${hours12.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')} ${period}`;
-
-        return time12hr;
+    const extractTime = (inputString) => {
+        const dateTimeObj = new Date(inputString)
+        const hours = dateTimeObj.getHours()
+        const minutes = dateTimeObj.getMinutes()
+        const seconds = dateTimeObj.getSeconds()
+        const period = hours >= 12 ? 'PM' : 'AM'
+        const hours12 = hours % 12 || 12
+        const time12hr = `${hours12.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')} ${period}`
+        return time12hr
     }
-
-
 
     return (
         <>
-
             <MetaData title="Application Details" />
-            <div className='bg-[#F1F2F4] min-h-screen pt-14 md:px-20 px-3 text-[#7A1515]'>
-                {
-                    loading ?
 
-                        <Loader />
-
-                        :
-
-                        <div>
-                            <div className='py-3  text-2xl md:text-4xl'>Application #{id}</div>
-
-                            <div className='pt-4 pb-3'>
-                                <p className='text-2xl pb-2'>Job Details:</p>
-                                <div>
-                                    <ul>
-                                        <li className='flex gap-4 items-center'>Role: <div>{applicationDetails.job.title}</div></li>
-                                        <li className='flex gap-4 items-center'>Company: <div>{applicationDetails.job.companyName}</div></li>
-                                        <li className='flex gap-4 items-center'>Location: <div>{applicationDetails.job.location}</div></li>
-                                        <li className='flex gap-4 items-center'>Experience: <div>{applicationDetails.job.experience}</div></li>
-                                    </ul>
-                                </div>
-                            </div>
-
+            <div className='bg-[#F5F6F8] min-h-screen md:px-8 text-black flex items-center justify-center'>
+                <div className='container mx-auto bg-white rounded-lg shadow-lg md:px-12 py-4 pb-8'>
+                    <div className='py-4 text-center'>
+                        <p className='text-4xl font-semibold'>Application to {applicationDetails.job.companyName}</p>
+                    </div>
+                    <div className='flex flex-col md:flex-row gap-6'>
+                        {/* Job Details Column */}
+                        <div className='flex-1'>
                             <div className='pt-4 pb-6'>
-                                <p className='text-2xl pb-2'>Applicants Details:</p>
-                                <div>
-                                    <ul>
-                                        <li className='flex gap-4 items-center'>Name: <div>{applicationDetails.applicant.name}</div></li>
-                                        <li className='flex gap-4 items-center'>Email: <div>{applicationDetails.applicant.email}</div></li>
-                                        <li className='flex gap-4 items-center'>Resume: <Link path="_blank" to={applicationDetails.applicantResume.url} target="_blank"
-                                            rel="noreferrer" className='text-blue-500 underline cursor-pointer'>{applicationDetails.applicant.name} resume</Link></li>
-                                    </ul>
-                                </div>
-
+                                <p className='text-2xl font-semibold pb-2 text-[#7A1515]'>Job Details:</p>
+                                <ul className='list-disc pl-5 space-y-2'>
+                                    <li className='flex items-center gap-4'>Role: <span>{applicationDetails.job.title}</span></li>
+                                    <li className='flex items-center gap-4'>Company: <span>{applicationDetails.job.companyName}</span></li>
+                                    <li className='flex items-center gap-4'>Location: <span>{applicationDetails.job.location}</span></li>
+                                    <li className='flex items-center gap-4'>Experience: <span>{applicationDetails.job.experience}</span></li>
+                                </ul>
                             </div>
-
-                            <div className='pt-2 pb-2'>
-                                <div className='flex gap-3  items-center text-xl'>Status: <span className={`${applicationDetails.status === "pending" ? "text-blue-600" :
-                                        applicationDetails.status === "rejected" ? "text-red-600" : "text-green-600"
-
-                                    } font-medium`}>{toUpperFirst(applicationDetails.status)}</span> </div>
-
-                            </div>
-
-
-                            <div className='pt-2 pb-2'>
-                                <div className='flex gap-3  items-center text-xl'>
-                                    Application Created At: {
-                                        convertDateFormat(applicationDetails.createdAt.substr(0, 10))} on {extractTime(applicationDetails.createdAt)}</div>
-
-                            </div>
-
-
-
-                            <div className='py- pb-40 '>
-
-                                {!loading? <button onClick={()=>{
-                                    deleteApplicationHandler()
-                                }} className='bg-red-600 hover:bg-red-800 py-2.5 text-sm  px-8 font-medium'>
-
-                                    Delete Application
-
-                                </button>
-                                    :
-                                <button className='bg-red-600 py-2   px-4 flex items-center font-bold justify-center '>
-
-                                    <TbLoader2 className='animate-spin mx-16' size={23} />
-
-                                </button>}
-
-                            </div>
-
-
-
-
                         </div>
-                }
 
+                        {/* Applicant Details Column */}
+                        <div className='flex-1'>
+                            <div className='pt-4 pb-6'>
+                                <p className='text-2xl font-semibold pb-2 text-[#7A1515]'>Applicant Details:</p>
+                                <ul className='list-disc pl-5 space-y-2'>
+                                    <li className='flex items-center gap-4'>Name: <span>{applicationDetails.applicant.name}</span></li>
+                                    <li className='flex items-center gap-4'>Email: <span>{applicationDetails.applicant.email}</span></li>
+                                    <li className='flex items-center gap-4'>Resume: <Link to={applicationDetails.applicantResume.url} target="_blank" rel="noreferrer" className='text-blue-500 underline'>{applicationDetails.applicant.name}'s resume</Link></li>
+                                </ul>
+                            </div>
 
+                            <div className='pt-2 flex gap-3'>
+                                <p className='text-xl font-semibold'>Status: <span className={`${applicationDetails.status === "pending" ? "text-[#7A1515]" :
+                                    applicationDetails.status === "rejected" ? "text-red-600" : "text-green-600"} font-medium`}>{toUpperFirst(applicationDetails.status)}</span></p>
+                            </div>
 
+                            <div className='pt-2 flex gap-3'>
+                                <p className='text-s font-medium'>Application Created At: <span>{convertDateFormat(applicationDetails.createdAt.substr(0, 10))} on {extractTime(applicationDetails.createdAt)}</span></p>
+                            </div>
 
+                            <div className='py-4'>
+                                {loading ? (
+                                    <button className='bg-red-600 py-2 px-4 flex items-center font-bold justify-center'>
+                                        <TbLoader2 className='animate-spin mx-16' size={23} />
+                                    </button>
+                                ) : (
+                                    <button onClick={deleteApplicationHandler} className='text-white bg-[#7A1515] hover:bg-[#874444] py-2.5 text-m px-8 font-semibold'>
+                                        Cancel Application
+                                    </button>
+                                )}
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-
         </>
     )
 }
