3f7ad9d3f03eacfdd43b3ec380b67ba73af6e924
CharlotteTaboada <charlotte.taboada@cit.edu>
2024-08-05 03:28:02 +0800

    jobdetails draft pt2

--- frontend/src/pages/JobDetails.jsx
+++ frontend/src/pages/JobDetails.jsx
@@ -100,12 +100,13 @@ export const JobDetails = () => {
                     />
                   </div>
                   <div className='md:ml-6'>
-                    <h1 className='text-2xl md:text-3xl font-bold'>{jobDetails.title}</h1>
-                    <h2 className='text-lg md:text-xl font-semibold mt-2 pr-2'>at {jobDetails.companyName } &nbsp;
-                    <span className={`px-2 py-1 rounded ${ getEmploymentTypeClass (jobDetails.employmentType)}`}>
-                      {capitalizeWords(jobDetails.employmentType)}
-                    </span>
-                    </h2>
+                    <div class="flex space-x-3 "> 
+                      <div> <h1 className='text-3xl md:text-4xl font-bold'>{jobDetails.title}</h1></div>
+                      <div className='py-2'> <span className={`px-1 py-1 flex-center rounded ${ getEmploymentTypeClass (jobDetails.employmentType)}`}>
+                      {capitalizeWords(jobDetails.employmentType)} 
+                    </span></div>
+                    </div>
+                    <h2 className='text-lg md:text-xl font-semibold mt-2 pr-2'>at {jobDetails.companyName }</h2>
                     
                     {isLogin && matchPercentage !== null && (
                     <p className='text-lg mt-2 text-blue-700'>
